12ceaddfe5dbf9c53ab25e5bd25ef2656506270d
AlexNik13 <4pnav21@gmail.com>
2021-12-07 10:12:41 +0200

    create local git

---
+++ src/main/java/com/hard/comhard/ComhardApplication.java
@@ -0,0 +1,13 @@
+package com.hard.comhard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComhardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComhardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hard/comhard/ComhardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hard.comhard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComhardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
